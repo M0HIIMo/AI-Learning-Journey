5d672d55be94834d63dcd22d54660a66647afb4b
mohdshmoslemi <mohdsh.1382.moslemi@gmail.com>
2026-08-14 12:41:16 +0330

    Day 2: Simple Calculator With Four Operations

---
+++ calculator.py
@@ -0,0 +1,30 @@
+print("---- My Smart Calculator ----")
+
+# گرفتن دو عدد از کاربر
+first_number = int(input("Enter First Number: "))
+second_number = int(input("Enter Second Number: "))
+
+# تبدیل رشته به عدد (خیلی مهم!!)
+# number_one = int(first_number)
+# number_two = int(second_number)
+
+# محاسبه جمع
+# result = number_one + number_two
+sum_result = first_number + second_number
+
+print("The Result Is: ", sum_result)
+
+# محاسبه تفریق
+subtract_result = first_number - second_number
+
+print("The Result Is: ", subtract_result)
+
+# محاسبه ضرب
+multiply_result = first_number * second_number
+
+print("The Result Is: ", multiply_result)
+
+# محاسبه تقسیم
+divide_result = first_number / second_number
+
+print("The Result Is: ", divide_result)
