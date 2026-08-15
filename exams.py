0c3490fd2b9732342ee039766c6b74f8100c0cdf
mohdshmoslemi <mohdsh.1382.moslemi@gmail.com>
2026-08-15 11:16:45 +0330

    Day 5: Daily Exam

---
+++ exams.py
@@ -0,0 +1,33 @@
+# total = 0
+# for i in range(2, 6):
+#     total = total + i
+# print(total)
+
+# count = 1
+# while count < 4:
+#     print(count)
+#     count = count + 1
+    
+# for i in range(3):
+#     print(f"Round {i + 1}")
+    
+# trying = 0
+# while trying < 3:
+#     trying = trying + 1
+# print(trying)
+
+# number = 0
+# for i in range(5):
+#     number = number + 1
+#     print(number)
+    
+# for i in range(5):
+#     print(i+1)
+    
+# for i in range(1, 6):
+#     print(i)     
+
+total = 10
+total = total - 3
+total = total - 2
+print(total)   
